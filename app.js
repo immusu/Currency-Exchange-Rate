f5f9231da598eda6451fd3a1036d23b5bfcb73e2
Md. Imamul Mursalin Sujoy <imamulmursalinsujoy@gmail.com>
2025-07-17 23:01:24 +0600

    updated

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.get('/', async (req, res) => {
       from: '',
       to: '',
       result: null,
+      showAllRates: false
     });
   } catch (error) {
     console.error(error);
@@ -27,6 +28,28 @@ app.get('/', async (req, res) => {
   }
 });
 
+
+app.get('/all-rates', async (req, res) => {
+  try {
+    const baseCurrency = req.query.base || 'USD';
+    const response = await axios.get(`https://v6.exchangerate-api.com/v6/48c6542721f721c26ea0d88d/latest/${baseCurrency}`);
+    const rates = response.data.conversion_rates;
+    
+    res.render('index', {
+      rates,
+      base: response.data.base_code,
+      amount: '',
+      from: '',
+      to: '',
+      result: null,
+      showAllRates: true,
+      selectedBase: baseCurrency
+    });
+  } catch (error) {
+    console.error(error);
+    res.send('Error fetching exchange rates.');
+  }
+});
 
 app.get('/convert', async (req, res) => {
   try {
@@ -45,6 +68,7 @@ app.get('/convert', async (req, res) => {
       from,
       to,
       result: convertedAmount.toFixed(2),
+      showAllRates: false
     });
   } catch (error) {
     console.error(error);
